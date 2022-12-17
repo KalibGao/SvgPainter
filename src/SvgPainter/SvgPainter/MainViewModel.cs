0a4114e4a75b178b59904e31df2a7218be9d4c17
Kalib.Gao <gaoziyin@vip.163.com>
2022-12-17 10:22:55 +0800

    feature: add project

---
+++ src/SvgPainter/SvgPainter/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SvgPainter
+{
+    [ObservableObject]
+    public partial class MainViewModel
+    {
+        [ObservableProperty]
+        private string? _svgText;
+
+        partial void OnSvgTextChanged(string? value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return;
+
+            try
+            {
+                SvgData = Geometry.Parse(value);
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        [ObservableProperty]
+        private Geometry? _svgData;
+    }
+}
---
+++ src/SvgPainter/SvgPainter/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace SvgPainter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new MainViewModel();
+
+            InitializeComponent();
+        }
+    }
+}
